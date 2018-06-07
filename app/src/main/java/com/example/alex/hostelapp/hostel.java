ba46b2c23612ad0db3966475d35ecb1b730e4709
Alexogendo <alexnyabuto8@gmail.com>
2018-06-07 13:59:48 +0630

    Initial commit

---
+++ app/src/main/java/com/example/alex/hostelapp/hostel.java
@@ -0,0 +1,13 @@
+package com.example.alex.hostelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hostel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hostel);
+    }
+}
